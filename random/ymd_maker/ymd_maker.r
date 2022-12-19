fc6d9f87a62706694b381efa8b67ff1c30289291
mahmudstat <almahmud.sbi@gmail.com>
2022-12-19 20:29:56 +0600

    Add ymd maker whole year

---
+++ random/ymd_maker/ymd_maker.r
@@ -0,0 +1,14 @@
+times = c(31,28,31,30,31,30,31,31,30,31,30,31)
+
+ymd <- data.frame(Month = rep(1:12, times = times))
+
+View(ymd)
+
+Day <- c()
+
+for (i in times) {
+  Day = c(Day, 1:i)
+}
+
+ymd$Day = Day
+
