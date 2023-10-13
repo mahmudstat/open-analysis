b51e8d925c545ebf3eeb386e446c8c0faa0bf9b9
Abdullah Al Mahmud <mahmud@pop-os.localdomain>
2023-10-13 08:38:29 +0600

    add dist map

--- random/bd_weather.R
+++ random/bd_weather.R
@@ -7,19 +7,23 @@ bd_division_weather <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-
 
 View(bd_division_weather)
 
-# Rename columns
+bd_dist_names <- read_csv("data/bd_districts.csv")
+
+View(bd_dist_names)
+
+# Bar Plot
 
 bd_division_weather %>% group_by(City) %>% 
   summarise(TotRain = sum(Rainfall_mm)) %>% 
   mutate(City = factor(City, levels = City[order(TotRain)])) %>% 
   ggplot(aes(City, TotRain, fill = City))+
   scale_fill_brewer(palette = "PiYG")+
-  geom_bar(stat = "identity")+
+  geom_bar(stat = "identity", width = 0.8)+
   coord_flip()+
   theme(legend.position = "none",
         axis.title = element_text(face="bold"))+
   geom_text(aes(label=TotRain), vjust=1, color="black", hjust = 1,
-            position = position_dodge(0.5), size=4)+
+            position = position_dodge(0.5), size=3.5)+
   labs(y = "Rainfall", 
        x = "City", 
        title = "Yearly Rainfall (mm) in Bangladesh Divisional Cities")
@@ -45,9 +49,43 @@ TotR <- bd_division_weather %>% group_by(City) %>%
 
 bd_div$Rain <- TotR$TotRain[c(1:4, 6:8)]
 
+
 spplot(bd_div, "Rain", col.regions = brewer.pal(n = 7, name = "YlGn"),
-       cuts=6, col='transparent', main='Rainfall in Bnagladesh Cities',
+       cuts=6, col='transparent', main='Annaul Rainfall  (in mm) in Bnagladesh',
        sub='Source: WMO & BMD', scales=list(draw=T))
+
+# District Rainfall
+
+bd_dist_rain <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6g3f0KJDt19di48bx55E_mTZZAtrop8psWbIUk4hD0YiVO4ivQHUw5yA3wTgb2_ddAn72Hut_GkuR/pub?gid=272007487&single=true&output=csv")
+
+View(bd_dist_rain)
+
+# See top five 
+
+bd_dist_rain %>% arrange(Rainfall18_m) %>% top_n(5)
+
+# More than 2k
+
+bd_dist_rain %>% filter(Rainfall18_m>2000)
+
+# District Map
+
+bd_dist_map <- readRDS("data/gadm36_BGD_2_sp.rds")
+
+bd_dist_map@data$NAME_2
+
+bd_dist_map$Rain <- bd_dist_rain$Rainfall17_mm
+
+View(bd_dist_rain)
+
+spplot(bd_dist_map, "Rain", col.regions = brewer.pal(n = 5, name = "PuBuGn"),
+       cuts=4, col='transparent', main='Annaul Rainfall  (in mm) in Bnagladesh',
+       sub='Source: WMO & BMD (2017)', scales=list(draw=T))
+
+# col.regions = heat.colors(8) another option
+
+bd_dist_map@data$NAME_2
+bd_dist_rain$District
 
 # Fortified data
 
