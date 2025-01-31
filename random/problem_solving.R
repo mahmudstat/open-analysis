49a8ffbe136902772d249c4b33d55db1dc39e769
mahmud <almahmud.sbi@gmail.com>
2025-01-31 21:20:25 +0600

    add 5!

---
+++ random/problem_solving.R
@@ -0,0 +1,21 @@
+# Write a for loop to calculate the factorial of a number n = 5
+
+# While loop for 5!, 5 factorial
+
+i <- 1
+f <- 1 
+while (i <=5) {
+  f <- i*f
+  i <- i + 1
+}
+f
+
+# For loop 
+
+f <- 1
+for (j in 1:5) {
+  f <- f*j
+}
+
+f
+
