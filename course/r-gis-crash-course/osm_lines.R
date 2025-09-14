1fe021b9f46da998feb4808a4b24abbbc61d2212
mahmudstat <almahmud.sbi@gmail.com>
2025-09-14 13:10:18 +0600

    Add Motijheel roads

--- course/r-gis-crash-course/osm_lines.R
+++ course/r-gis-crash-course/osm_lines.R
@@ -2,6 +2,8 @@
 
 library(osmdata)
 library(sf)
+library(ggplot2)
+
 
 # By AREA ####
 q <- opq("Farmgate, Bangladesh") %>% 
@@ -31,4 +33,37 @@ sylbb <- opq(bbox = syl) %>%
 
 syl_lines <- osmdata_sf(sylbb)$osm_lines
 
-plot(syl_lines)
+plot(syl_lines["name"]) # Takes
+
+# Add dummy attributes for illustration
+set.seed(123)
+syl_lines$safety <- sample(c("High", "Medium", "Low"), nrow(syl_lines), replace = TRUE)
+syl_lines$jam <- runif(nrow(syl_lines), 0, 1)   # traffic jam index 0–1
+
+library(ggplot2)
+
+# Plot roads colored by safety level
+ggplot(syl_lines) +
+  geom_sf(aes(color = safety), size = 1) +
+  scale_color_manual(values = c("High" = "green", "Medium" = "orange", "Low" = "red")) +
+  theme_minimal() +
+  ggtitle("Road Safety in Sylhet (Example Data)")
+
+# Motijheel
+
+motijheel <- getbb("Motijheel, Bangladesh")
+
+motijheel_bb <- opq(bbox = motijheel) %>%
+  add_osm_feature(key = "highway")
+
+motijheel_lines <- osmdata_sf(motijheel_bb)$osm_lines
+
+set.seed(100)
+motijheel_lines$saefty <- sample(c("High", "Low", "Medium"), nrow(motijheel_lines), replace = TRUE)
+
+# Plot roads colored by safety level
+ggplot(motijheel_lines) +
+  geom_sf(aes(color = saefty), size = 1) +
+  scale_color_manual(values = c("High" = "green", "Medium" = "orange", "Low" = "red")) +
+  theme_minimal() +
+  ggtitle("Road Safety in Motijhel (Simulated Data)")
